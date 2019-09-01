005c0b6be3e962d368d124b21e05299e358b0137
Luis Guilherme Pelin Martins <lgpelin92@gmail.com>
2019-09-01 18:09:57 -0300

    FEA : add crud to client model

--- src/helpers/menu.js
+++ src/helpers/menu.js
@@ -5,14 +5,19 @@ const clients = require("./../models/clients")
 const food = require("./../models/food")
 
 const exitString = "quit"
+const returnMenu = "return"
 
 const simpleQuestion = stringQuestion => {
 	return new Promise((resolve, reject) => {
 		rl.question(`${stringQuestion}\n`, answer => {
 			if (answer == exitString) {
 				rl.close()
+			} else if (answer == returnMenu) {
+				console.log("Retornando para o Menu Principal \n")
+				mainMenu()
+			} else {
+				resolve(answer)
 			}
-			resolve(answer)
 		})
 	})
 }
@@ -27,7 +32,11 @@ var recursiveQuestion = (
 			function(answer) {
 				if (answer == exitString) {
 					rl.close()
+				} else if (answer == returnMenu) {
+					console.log("Retornando para o Menu Principal \n")
+					mainMenu()
 				}
+
 				if (answer == escapeString) return resolve(arrayOfAnswers)
 				else {
 					arrayOfAnswers.push(answer)
@@ -45,7 +54,9 @@ var recursiveQuestion = (
 	})
 }
 
-const welcomeMap = async () => {
+const mainMenu = async () => {
+	console.log("\n ## Menu Principal ## \n")
+
 	const welcomeQuestion = await simpleQuestion(
 		"Escolha a opção desejada, digite \n 1 para Clientes, \n 2 para Consultas, \n 3 para cardapios"
 	)
@@ -59,7 +70,11 @@ const welcomeMap = async () => {
 	} else if (welcomeQuestion == 3) {
 		console.log("welcome 3 ")
 		await cardapiosMap()
+	} else {
+		console.log("Opção invalida por favor tente novamente")
+		await mainMenu()
 	}
+	await mainMenu()
 }
 const addClientMap = async () => {
 	const name = await simpleQuestion("Qual o nome do cliente ?")
@@ -87,15 +102,79 @@ const addClientMap = async () => {
 const clientsMap = async () => {
 	console.log("\n ## Clientes ## \n")
 	const clientsQuestion = await simpleQuestion(
-		"Escolha a opção desejada, digite \n 1 para Criar novo cliente, \n 2 para visualizar clientes existentes, \n 3 para editar clientes existentes, \n 4 para excluir clientes existentes \n "
+		"Escolha a opção desejada, digite \n 1 para Criar novo cliente, \n 2 para visualizar clientes existentes, \n 3 para procurar clientes existentes (pelo id), \n 4 para excluir clientes existentes, \n 5 para editar clientes existentes (pelo id) "
 	)
 	if (clientsQuestion == 1) {
 		await addClientMap()
 	} else if (clientsQuestion == 2) {
-		console.log(clients.clients)
+		console.log(JSON.stringify(clients.clients, null, 2))
 	} else if (clientsQuestion == 3) {
-		console.log("editar cliente ainda não implementado")
+		let idClient = await simpleQuestion(
+			"Qual id do cliente que deseja consultar ?"
+		)
+		let client = await clients.findClientById(idClient)
+		if (client) {
+			console.log(JSON.stringify(client, null, 2))
+		} else {
+			console.log(
+				`cliente não encontrado tente novamente, ou digite ${returnMenu} para retornar ao menu principal ou '${exitString}' para sair \n`
+			)
+		}
+	} else if (clientsQuestion == 4) {
+		v
+		let deletedClient = await clients.deleteClient(idClient)
+		if (deletedClient) {
+			console.log("Cliente : \n")
+			console.log(JSON.stringify(deletedClient, null, 2))
+			console.log("Deletado com sucesso! \n")
+		} else {
+			console.log(
+				`cliente não encontrado tente novamente, ou digite ${returnMenu} para retornar ao menu principal ou '${exitString}' para sair \n`
+			)
+		}
+	} else if (clientsQuestion == 5) {
+		const id = await simpleQuestion("Qual id de usuario deseja alterar ? ")
+		const name = await simpleQuestion(
+			"Deseja alterar o nome do cliente ? Caso Contrario Pressione 'Enter' "
+		)
+		const adress = await simpleQuestion(
+			"Deseja alterar o endereço do cliente ? Caso Contrario Pressione 'Enter' "
+		)
+		const phone = await simpleQuestion(
+			"Deseja alterar o endereço do telefone ? Caso Contrario Pressione 'Enter' "
+		)
+		const email = await simpleQuestion(
+			"Deseja alterar o endereço do email ? Caso Contrario Pressione 'Enter' "
+		)
+		const bornIn = await simpleQuestion(
+			"Qual data de nascimento do cliente ?"
+		)
+		const changeRestrictsFoods = await simpleQuestion(
+			"Deseja alterar as restrições alimentares ? Se sim digite 'S'  "
+		)
+		let newRestrictsFoods = false
+		if (changeRestrictsFoods == "s") {
+			newRestrictsFoods = await recursiveQuestion(
+				"Se houver adição de restrição alimentar por favor digite o nome do alimento",
+				"N"
+			)
+		}
+		let editedClient = {
+			id: id,
+			name: name ? name : false,
+			adress: adress ? adress : false,
+			phone: phone ? phone : false,
+			email: email ? email : false,
+			bornIn: bornIn ? bornIn : false,
+			newRestrictsFoods: newRestrictsFoods ? newRestrictsFoods : false
+		}
+		clients.editClient(editedClient)
+		console.log(clients.clients)
+	} else {
+		console.log("Opção invalida por favor tente novamente")
+		await clientsMap()
 	}
+	await clientsMap()
 }
 
 const cardapiosMap = async () => {
@@ -120,7 +199,13 @@ const cardapiosMap = async () => {
 		console.log(meals)
 
 		console.log(`com a restrição dos alimentos ${restrictsFoods}\n`)
+	} else if (cardapiosQuestion == 2) {
+		console.log("opção ainda não implementada")
+	} else {
+		console.log("Opção invalida por favor tente novamente")
+		await clientsMap()
 	}
+	await cardapiosMap()
 }
 
 const consultasMap = async () => {}
@@ -129,10 +214,13 @@ const interface = async () => {
 	console.log(`Bem vindo ao iNutri DTI \n`)
 
 	console.log(
-		`Caso deseje encerrar o programa em qualquer momento basta digitar a palavra '${exitString}' \n`
+		`Caso deseje encerrar o programa em qualquer momento basta digitar '${exitString}' \n`
+	)
+	console.log(
+		`Caso deseje retornar ao menu principal em qualquer momento basta digitar '${returnMenu}' \n`
 	)
 
-	await welcomeMap()
+	await mainMenu()
 
 	rl.close()
 }
--- src/models/clients.js
+++ src/models/clients.js
@@ -1,5 +1,6 @@
 const clients = [
 	{
+		id: 0,
 		name: "Maria Luzia",
 		adress: "Rua Maracuja 37",
 		phone: "32103210",
@@ -12,7 +13,7 @@ const clients = [
 				weight: "",
 				percentualFatMass: "",
 				pacientFeedback: "",
-				restrictedFood: []
+				restrictedFood: ["arroz"]
 			}
 		]
 	}
@@ -29,9 +30,37 @@ const addClient = (name, adress, phone, email, bornIn, restrictedFood = []) => {
 		restrictedFood: restrictedFood
 	}
 
-	//ADD HANDLE TYPE OF INFOS
-
 	return clients.push(newClient)
+}
+
+const findClientById = id => {
+	let client
+	clients.forEach(e => {
+		if (e.id == id) client = e
+	})
+	return client ? client : false
+}
+
+const deleteClient = id => {
+	return clients.splice(id, 1)
+}
+
+const editClient = editedClient => {
+	let oldClient = findClientById(editedClient.id)
+	let newClient = {
+		id: editedClient.id,
+		name: editedClient.name ? editedClient.name : oldClient.name,
+		adress: editedClient.adress ? editedClient.adress : oldClient.adress,
+		phone: editedClient.phone ? editedClient.phone : oldClient.phone,
+		email: editedClient.email ? editedClient.email : oldClient.email,
+		bornIn: editedClient.bornIn ? editedClient.bornIn : oldClient.bornIn,
+		newRestrictsFoods: editedClient.newRestrictsFoods
+			? editedClient.newRestrictsFoods
+			: oldClient.newRestrictsFoods,
+		appointments: oldClient.appointments
+	}
+	clients[editedClient.id] = newClient
+	return newClient
 }
 
 addClient("luis", "meu endereçõ loko", 682344, "oi@oi.com", "04/05/1992", [
@@ -40,5 +69,8 @@ addClient("luis", "meu endereçõ loko", 682344, "oi@oi.com", "04/05/1992", [
 
 module.exports = {
 	clients: clients,
-	addClient: addClient
+	addClient: addClient,
+	findClientById: findClientById,
+	deleteClient: deleteClient,
+	editClient: editClient
 }
